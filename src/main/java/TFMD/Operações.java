3cbb2648cb89626e8ef8f014dead87e94c893338
JoatanMarques <31866094+JoatanMarques@users.noreply.github.com>
2021-06-23 13:00:33 -0300

    Criação do design inicial da interface

---
+++ src/main/java/TFMD/Operações.java
@@ -0,0 +1,31 @@
+public class Operações extends Comodos{
+    private double m2;
+    private double resultado;
+
+    public Operações(double largura, double comprimento, double valor) {
+        setM2();
+        setLargura(largura);
+        setComprimento(comprimento);
+        setValor(valor);
+    }
+
+    public double getM2() {
+        return this.m2;
+    }
+
+    private void setM2() {
+        this.m2 = getLargura()*getComprimento();
+    }
+
+    public double getResultado() {
+        return this.resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
+    private void contas(){
+        setResultado(this.m2 * getValor());
+    }
+}
